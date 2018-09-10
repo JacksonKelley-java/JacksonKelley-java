78b5b687c85e5d9559809ac3d2167006c4226c96
amerk65584 <amerk65584@gmail.com>
2018-09-10 08:54:39 -0700

    Draw boxes.

---
+++ DrawBoxes/src/DrawBoxes.java
@@ -0,0 +1,21 @@
+public class DrawBoxes {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Draw Boxes!");
+        drawBox();
+        drawBox();
+        drawBox();
+        System.out.println("Project Complete.");
+    }
+
+    public static void drawBox(){
+        System.out.println();
+        System.out.println("+---------+");
+        System.out.println("|         |");
+        System.out.println("|         |");
+        System.out.println("+---------+");
+
+
+    }
+
+}
