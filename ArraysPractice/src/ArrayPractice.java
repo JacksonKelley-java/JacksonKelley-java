62ab476305f9bd9e6c25f7875db496d2c39d06d6
amerk65584 <amerk65584@gmail.com>
2018-12-08 09:46:56 -0800

    array practice worksheet 4.1

--- ArraysPractice/src/ArrayPractice.java
+++ ArraysPractice/src/ArrayPractice.java
@@ -28,8 +28,40 @@ public class ArrayPractice {
         //Exercise 3a
             System.out.println(arrayOfNumbers[(arrayOfNumbers.length-1)]);
         //Exercise 3b
-        
-
+        Arrays.toString(arrayOfNumbers2[arrayOfNumbers2.length/2]);
+        //Exercise 3c
+        Scanner inputArray = new Scanner(System.in);
+        for (int i = 0; i <4; i++) {
+            arrayOfNumbers2[i] = arrayOfNumbers2[i]-1;
+        }
+        //exercise 3d
+        int[] even = new int[499];
+        for (int i = 0; int i < even.length-1; i++) {
+            int x =0;
+            if (x % 2 = 0) {
+                even[i]=x;
+            } else {
+                x++
+            }
+        }
+        //exercise 3f
+        int x = even[100];
+        return x;
+        //exercise 3g
+        int y = even[278];
+        return y;
+        int z = even[456];
+        return z;
+        //exercise 3h
+        int[] skrt = new int[10];
+        for (int i = 0; i <skrt.length; i++) {
+            skrt[i] = i-4;
+        }
+        //exercise 3i
+        int [] word = new int[int length];
+        for (int i = 0; i < word.length; i++) {
+            word[i] = i - length;
+        }
 
 
     }
