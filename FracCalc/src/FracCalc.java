32699c4668b22d1acb728a8ff3c807ffece6b97c
amerk65584 <amerk65584@gmail.com>
2018-12-11 09:20:38 -0800

    frac calc

---
+++ FracCalc/src/FracCalc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class FracCalc {
+
+    public static void main(String[] args) {
+        Scanner userInput =  new Scanner(System.in);
+        System.out.println("Input? To begin, type Start.");
+        String input = userInput.next();
+
+        while ((input.equals("Start"))) {
+            System.out.println("Enter input");
+
+            System.out.println("testing");
+        }
+    }
+
+}
