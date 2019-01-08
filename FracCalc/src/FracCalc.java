97e4e6b90131fcb701611f7fae613d42935238a1
amerk65584 <amerk65584@gmail.com>
2019-01-08 09:20:28 -0800

    frac calc

--- FracCalc/src/FracCalc.java
+++ FracCalc/src/FracCalc.java
@@ -8,18 +8,39 @@ public class FracCalc {
         String input = userInput.next();
 
         while ((!input.equals("quit"))) {
-            System.out.println("Enter 3 words of input. Enter quit to exit.");
+            System.out.println("Enter 3 words of input.");
             Scanner threeTokens = new Scanner(System.in);
             System.out.println(threeTokens.next());
             System.out.println(threeTokens.next());
             System.out.println(threeTokens.next());
-            System.out.println("Play Again?");
+            System.out.println("Play Again? Enter quit to exit.");
             input = userInput.next();
             if (input.equals("quit")) {
                 System.out.println("Thanks for coming, but the program has finished.");
             }
 
 
+
+
         }
     }
 }
+
+/*
+
+public class Looper {
+    public static void main(String[] args){
+        //stuff
+    }
+    Scanner userInput = new Scanner(System.in);
+        String var1 = "";
+        String var2 = "";
+        String var3 = "";
+
+
+
+
+
+}
+
+*/
